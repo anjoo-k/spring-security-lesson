7f1552fd903e24793971288fd74b6eb4969d502f
anjoo-k <knjou92@naver.com>
2025-10-18 11:18:55 +0900

    feat: heath message update

--- src/main/java/org/kosa/myproject/controller/HealthController.java
+++ src/main/java/org/kosa/myproject/controller/HealthController.java
@@ -8,7 +8,7 @@ public class HealthController {
 
     @GetMapping("/api/health")
     public String health() {
-        return "OK!!!";
+        return "OK 성공성공성공";
     }
 }
 /*******************Security 설정에서 꼭 추가하세요***************************/
